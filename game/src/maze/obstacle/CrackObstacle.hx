686ae361a9bd013e7e2506d770e3b78fc0a6c5cb
Luke Cann <luke@lc-apps.co.uk>
2025-08-28 19:19:48 +0100

    Stepping on cracks decreases luck

--- game/src/maze/obstacle/CrackObstacle.hx
+++ game/src/maze/obstacle/CrackObstacle.hx
@@ -3,6 +3,8 @@ package maze.obstacle;
 import js.html.CanvasRenderingContext2D;
 
 class CrackObstacle extends AbstractObstacle {
+	private var triggered:Bool = false;
+
     public function new(room:Room, x:Float, y:Float, w:Float, h:Float) {
         super(room);
 		aabb.w = w;
@@ -11,7 +13,16 @@ class CrackObstacle extends AbstractObstacle {
 		this.y = y;
 	}
 
-	public function update(s:Float) {}
+	public function update(s:Float) {
+		var playerOverlaps:Bool = room.player.aabb.overlaps(aabb);
+
+		if(!triggered && playerOverlaps){
+			triggered = true;
+			room.luck -= 10;
+		}
+
+		triggered = playerOverlaps;
+	}
 
 	public function draw(c:CanvasRenderingContext2D) {
 		c.fillStyle = "#0F0";
